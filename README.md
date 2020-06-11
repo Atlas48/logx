280c9e2192023d06ad332dcdff3ce257c94cd0e2
Atlas <AtlasJan@gmx.com>
2020-06-11 19:24:13 +0100

    slight modifications

--- README.md
+++ README.md
@@ -3,6 +3,16 @@ Simple, pretty, traffic-light logger
 
 logx is an incredably lightweight traffic-light pattern logger, only a single, 7-element object in memory
 
+## Example
+
+```js
+logx=require('logx');
+
+logx.err('This is an error');
+logx.inf('The process ran succesfully');
+logx.wrn('You forgot to brush your teeth.');
+```
+
 ## Commands
 **Common Functionality:** all of the functions have an optional second argument that redirects to `stdout` instead of `stderr`
 when it is set to true.
--- logx.js
+++ logx.js
@@ -6,22 +6,22 @@ module.exports.prefix = {
 };
 module.exports.wrn = function(out, b){
   if (b) {
-    console.log(module.exports.prefix.wrn + " " + out);
+    process.stdout.write(module.exports.prefix.wrn + " " + out);
   } else {
-    console.error(module.exports.prefix.wrn + " " + out);
+    process.stderr.write(module.exports.prefix.wrn + " " + out);
   }
 };
 module.exports.err = function(out, b){
   if (b) {
-    console.log(module.exports.prefix.err + " " + out);
+    process.stdout.write(module.exports.prefix.err + " " + out);
   } else {
-    console.error(module.exports.prefix.err + " " + out);
+    process.stderr.write(module.exports.prefix.err + " " + out);
   }
 };
 module.exports.inf = function(out, b){
   if (b) {
-    console.log(module.exports.prefix.inf + " " + out);
+    process.stdout.write(module.exports.prefix.inf + " " + out);
   } else {
-    console.error(module.exports.prefix.inf + " " + out);
+    process.stderr.write(module.exports.prefix.inf + " " + out);
   }
 };
